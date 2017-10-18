e967152bb12dde925f8470b6453235d00843377d
Tamas K. Papp <tkpapp@gmail.com>
2017-10-18 17:13:29 +0200

    Initial commit.

--- src/FlexDates.jl
+++ src/FlexDates.jl
@@ -1,5 +1,52 @@
 module FlexDates
 
-# package code goes here
+import Base: convert, isless, typemin, typemax, ==, eltype, show, hash
+
+export FlexDate
+
+struct FlexDate{E, T <: Integer}
+    Δ::T
+    function FlexDate{E}(Δ::T) where {E, T <: Integer}
+        @assert E isa Date
+        new{E, T}(Δ)
+    end
+end
+
+show(io::IO, flexdate::FlexDate{E, T}) where {E, T} = 
+    print(io, "$(convert(Date, flexdate)) [$(E) + $(T)($(flexdate.Δ)) days]")
+
+function convert(::Type{Date}, flexdate::FlexDate{E}) where E
+    E + Dates.Day(flexdate.Δ)
+end
+
+function convert(::Type{FlexDate{E,T}}, date::Date) where {E, T}
+    FlexDate{E}(T(Dates.days(date - E)))
+end
+
+isless(x::FlexDate{E}, y::FlexDate{E}) where E = isless(x.Δ, y.Δ)
+
+isless(x::FlexDate{E1}, y::FlexDate{E2}) where {E1, E2} =
+    isless(convert(Date, x), convert(Date, y))
+
+isless(x::FlexDate{E}, y::Date) where E = isless(convert(Date, x), y)
+
+isless(x::Date, y::FlexDate{E}) where E = isless(x, convert(Date, y))
+
+typemin(::Type{FlexDate{E, T}}) where {E, T} = FlexDate{E, T}(typemin(T))
+
+typemax(::Type{FlexDate{E, T}}) where {E, T} = FlexDate{E, T}(typemax(T))
+
+(==)(x::FlexDate{E}, y::FlexDate{E}) where E = x.value == y.value
+
+(==)(x::FlexDate{E1}, y::FlexDate{E2}) where {E1, E2} =
+    convert(Date, x) == convert(Date, y)
+
+(==)(x::FlexDate{E}, y::Date) where E = convert(Date, x) == y
+
+(==)(x::Date, y::FlexDate{E}) where E = x == convert(Date, y)
+
+eltype(x::FlexDate{E, T}) where {E, T} = T
+
+hash(x::FlexDate, h::UInt) = hash(convert(Date, x), u)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,25 @@
 using FlexDates
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+E1 = Date(1980, 1, 1)
+E2 = Date(1990, 1, 1)
+
+D1 = Date(2001, 1, 8)
+D2 = Date(2007, 2, 9)
+
+F11 = FlexDate{E1, Int16}(D1)
+F12 = FlexDate{E2, Int32}(D1)
+
+F21 = FlexDate{E1, Int16}(D2)
+F22 = FlexDate{E2, Int32}(D2)
+
+F3 = FlexDate{E1}(0)
+F4 = FlexDate{E1}(5)
+F5 = FlexDate{E1}(-5)
+
+@test F11 == F12
+@test F21 == F22
+@test F11 ≠ F22
+@test F5 < F3 < F4
+@test F5 < E1 < F4
+@test F3 == E1
